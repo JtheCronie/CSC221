291a13cdf154e44023fcefef4f71e2c345f2210d
jason cronenwett <jasoncronenwett@MacBookPro.home.local>
2026-01-25 19:37:55 -0500

    added first name option

--- checkpoint2.cpp
+++ checkpoint2.cpp
@@ -15,13 +15,17 @@ int main()
     char firstInitial[] = "J";
     char middleInitial[] = "J";
     char lastInitial[] = "C";
+    std::string firstName;
     std::cout << "Please enter the initial of your first name:";
     std::cin >> firstInitial;
     std::cout << "Please enter the initial of your middle name:";
     std::cin >> middleInitial;
     std::cout << "Please enter the initial of your last name:";
     std::cin >> lastInitial;
-    std::cout << "Your initials are: " << firstInitial<< middleInitial<<lastInitial;
-
+    std::cout << "Your initials are: " << firstInitial<< middleInitial<<lastInitial<<
+std::endl;
+    std::cout << "Please enter your first name:";
+    std::cin >> firstName;
+    std::cout << "Your first name is : " << firstName;
     return 0;
 }
