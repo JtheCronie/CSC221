a13abbe1cbf1b59befb68ba7eec505630fc2be05
jason cronenwett <jasoncronenwett@MacBookPro.home.local>
2026-01-25 20:09:22 -0500

    Work hours calculator

---
+++ chapter_1_program_1-1.cpp
@@ -0,0 +1,22 @@
+ /* This program calculates the user's pay.*/
+
+ #include <iostream>
+
+ using namespace std;
+ int main()
+ {
+    double hours, rate, pay;
+
+    /* Get the number of hours worked.*/
+
+    std::cout << "How many hours did you work? ";
+    std::cin >> hours;
+    /* Get the hourly pay rate.*/
+    std::cout << "How much do you get paid per hour? ";
+    std::cin >> rate;
+    /* Calculate the pay.*/
+    pay = hours * rate;
+    /* Display the pay. */
+    std::cout << "You have earned $" << pay << endl;
+    return 0;
+ }
